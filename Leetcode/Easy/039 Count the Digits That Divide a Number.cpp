8b1364653caf18c23f190f3ddae1457b2824df17
Abhinav Singh <114523067+Abhinavsingh0404@users.noreply.github.com>
2024-10-14 10:33:41 +0530

    Add files via upload

---
+++ Leetcode/Easy/039 Count the Digits That Divide a Number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countDigits(int num) {
+        int count = 0;
+        int val = 0;
+        int temp = num;
+        while(temp>0){
+            val = temp%10;
+            if(val!=0 && num%val==0){
+                count++;
+            }
+            temp/=10;
+        }
+        return count;
+    }
+};
